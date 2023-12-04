b393db2ea1b025fb0cf648727adbe8702786c1d4
hxsggsz <vt.hugo.2021@gmail.com>
2023-12-03 21:21:26 -0300

    feat: create delete value function

--- src/game/main.js
+++ src/game/main.js
@@ -1,33 +1,49 @@
-import './styles.scss'
-import '../styles/base.scss'
-import { Select } from "domhelper.js"
+import './styles.scss';
+import '../styles/base.scss';
+import { Select } from 'domhelper.js';
 
-const inputs = new Select("input").getAllResults()
+const inputs = new Select('input').getAllResults();
 
 function restartGame() {
-	inputs.forEach((input) => {
-		input.value = ""
-	})
+  inputs.forEach((input) => {
+    input.value = '';
+  });
 
-	const firstInput = inputs[0]
-	firstInput.focus()
+  const firstInput = inputs[0];
+  firstInput.focus();
 }
 
 /**
- * 
+ *
  * @param {HTMLInputElement} currentInput - the current input
- * 
+ * @param {number} index - the index of inputs
+ *
  */
-function onInputChange(currentInput, index) {
-	const nextInput = inputs[index + 1];
-	if (currentInput.value.length && nextInput) {
-		nextInput.focus();
-		return;
-	}
+function onInputChange(ev, currentInput, index) {
+  const nextInput = inputs[index + 1];
+  if (currentInput.value.length && nextInput) {
+    nextInput.focus();
+    return;
+  }
 
-	restartGame()
+  restartGame();
 }
 
-inputs.forEach((input, index) => {
-  input.addEventListener("keydown", () => onInputChange(input, index))
-})
+function handleKeyDown(ev, currentInput, index) {
+  if (ev.key === 'Backspace') {
+    const prevInput = inputs[index - 1];
+    if (prevInput.value.length) {
+      prevInput.focus();
+      prevInput.value = '';
+    }
+  }
+}
+
+inputs.forEach((currentInput, index) => {
+  currentInput.addEventListener('input', (ev) =>
+    onInputChange(ev, currentInput, index)
+  );
+  currentInput.addEventListener('keydown', (ev) =>
+    handleKeyDown(ev, currentInput, index)
+  );
+});
